ce98a242580e2b36523330812c88ac4500f9bd3a
lhl <zhucezhiyong@126.com>
2013-09-03 14:18:56 +0800

    all sort algorithm

--- DataStructure/sortAlgorithm.cpp
+++ DataStructure/sortAlgorithm.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
 /*//////////////////////////////////////////////////////////////////////////
@@ -301,13 +302,198 @@ void merge_sort(int data[],int n){
 	free(buf);
 }
 
-void chose_sort(){
-
+void select_sort(int data[],int n){
+	int i;
+	int *p;
+	int temp;
+	for (i=0;i<n;i++)
+	{
+		p=min_element(data+i,data+n);
+		temp=*p;
+		*p=data[i];
+		data[i]=temp;
+	}
 }
 
-int main(){
-	int data[]={5,3,1,2,9,6};
-	tableInsert_sort(data,6);
-	showInts(data,6);
-	return 0;
+int* min_treeSelect(int data[],int l,int h){
+	if (l>h)
+	{
+		return NULL;
+	}
+	if (l==h)
+	{
+		return data+l;
+	}
+	int m=(h+l)/2;
+	int *pre;
+	int *tail;
+	pre=min_treeSelect(data,l,m);
+	tail=min_treeSelect(data,m+1,h);
+	
+	return *pre<*tail ? pre:tail;
 }
+
+void treeSelect_sort(int data[],int n){
+	int i;
+	int temp;
+	int *p;
+	for (i=0;i<n;i++)
+	{
+		p=min_treeSelect(data,i,n-1);
+		if (p!=(data+i))
+		{
+			temp=*p;
+			*p=data[i];
+			data[i]=temp;
+		}
+	}
+}
+
+void fromTop_adjustHeap(int heap[],int n,int top){
+	int i=top;
+	if (2*top+1<n && heap[i] > heap[2*top+1])
+	{
+		i= top*2+1;
+	}
+	if (2*top+2<n && heap[i] > heap[2*top+2])
+	{
+		i=top*2+2;
+	}
+	int temp;
+	if (i!=top)
+	{
+		temp=heap[top];
+		heap[top]=heap[i];
+		heap[i]=temp;
+		fromTop_adjustHeap(heap,n,i);
+	}
+}
+
+void heap_sort(int data[],int n){
+	//create heap
+	int i;
+	for (i=(n-2)/2;i>=0;i--)
+	{
+		fromTop_adjustHeap(data,n,i);
+	}
+
+	int temp;
+	for (i=n-1;i>0;i--)
+	{
+		temp=data[0];
+		data[0]=data[i];
+		data[i]=temp;
+
+		fromTop_adjustHeap(data,i,0);
+	}
+}
+
+void count_sort(int data[],int n){
+	int *buf=(int*)malloc(sizeof(int)*n);
+	int i;
+	int j;
+	int temp;
+
+	memset(buf,0,sizeof(int)*n);
+	for(i=0;i<n;i++){
+		temp=data[i];
+		for(j=i+1;j<n;j++){
+			if (temp>data[j])
+			{
+				buf[i]++;
+			}else{
+				buf[j]++;
+			}
+		}
+	}
+	
+	for (i=0;i<n;i++)
+	{
+		while (i!=buf[i])
+		{
+			temp=data[i];
+			data[i]=data[buf[i]];
+			data[buf[i]]=temp;
+
+			temp=buf[i];
+			buf[i]=buf[temp];
+			buf[temp]=temp;
+		}
+	}
+}
+
+
+struct node {
+	struct node *next;
+	int data;
+};
+
+typedef struct node Node;
+
+Node *makeNode(int data){
+	Node *nd=(Node*)malloc(sizeof(Node));
+	nd->next=NULL;
+	nd->data=data;
+	return nd;
+}
+
+int getkey(int data,int m){
+	int base=pow(10,m);
+	data/=base;
+	data%=10;
+	return data;
+}
+
+void distribution(int data[],int n,Node tail[],int m){
+	int i;
+	int key;
+	Node* pnode;
+	for (i=0;i<n;i++)
+	{
+		key=getkey(data[i],m);
+		pnode=makeNode(data[i]);
+		tail[key].next->next=pnode;
+		tail[key].next=pnode;
+	}
+}
+
+void collect(int data[],int n,Node nodes[]){
+	int i;
+	Node *p;
+	int j;
+	for (j=0,i=0;i<10 && j<n;i++)
+	{
+		p=nodes[i].next;
+		while (p!=NULL)
+		{
+			data[j++]=p->data;
+			nodes[i].next=p->next;
+			free(p);
+			p=nodes[i].next;
+		}
+	}
+}
+
+void linkedRadix_sort(int data[],int n){
+	int i;
+	int j;
+	Node nodes[10];
+	Node tail[10];
+	for (i=0;i<3;i++)
+	{
+		for (j=0;j<10;j++)
+		{
+			nodes[j].next=NULL;
+			tail[j].next=nodes+j;
+		}
+		distribution(data,n,tail,i);
+		collect(data,n,nodes);
+	}
+}
+
+//int main(){
+//	int data[]={65,93,21,12,39,56};
+//	linkedRadix_sort(data,6);
+//	showInts(data,6);
+//	return 0;
+// }
