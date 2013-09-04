7ea370802f6f31add857e6a5684349d756e8735b
lhl <zhucezhiyong@126.com>
2013-09-04 23:55:56 +0800

    some tree algorithm

--- DataStructure/Tree.cpp
+++ DataStructure/Tree.cpp
@@ -166,12 +166,176 @@ void showSet(int data[],int n){
 	}
 }
 
+struct hfmnode {
+	int w;
+	int parent;
+	int lchild;
+	int rchild;
+};
 
+typedef struct hfmnode *HfmTree;
+
+void selectMin2(HfmTree t,int n,int &i,int &j){
+	if (n<2)
+	{
+		i=j=-1;
+		return ;
+	}
+
+	int k;
+	i=j=-1;
+	for (k=0;k<n;k++)
+	{
+		if (-1!=t[k].parent)
+		{
+			continue;
+		}
+
+		if (-1==i || t[k].w < t[i].w)
+		{
+			j=i;
+			i=k;
+		}else if(-1==j || t[k].w < t[j].w){
+			j=k;
+		}
+	}
+
+}
+
+void hfmCode(int w[],int n,char *code[]){
+	//init hfm tree
+	HfmTree tree=(HfmTree)malloc(sizeof(struct hfmnode)*n*2);
+	int i;
+	for (i=0;i<n;i++)
+	{
+		tree[i].lchild=tree[i].rchild=tree[i].parent=-1;
+		tree[i].w=w[i];
+	}
+	for (i=n;i<n*2;i++)
+	{
+		tree[i].lchild=tree[i].rchild=tree[i].parent=-1;
+	}
+	//create hfm tree
+	int j,k;
+	for (i=n;i<2*n-1;i++)
+	{
+		selectMin2(tree,i,j,k);
+		tree[k].parent=tree[j].parent=i;
+		tree[i].lchild=k;
+		tree[i].rchild=j;
+		tree[i].w=tree[k].w+tree[j].w;
+	}
+	//coding hfm code
+	char *buf=(char*)malloc(n);
+	int bn=0;
+	for (i=0;i<n;i++)
+	{
+		code[i]=(char*)malloc(n);
+	}
+
+	for (i=0;i<n*2;i++)
+	{
+		tree[i].w=0;
+	}
+	i=2*n-2;
+	while (i!=-1)
+	{
+		switch(tree[i].w) {
+		case 0:
+			tree[i].w=1;
+			if (-1!=tree[i].lchild)
+			{
+				buf[bn++]='1';
+				i=tree[i].lchild;
+			}else if(-1==tree[i].rchild){
+				buf[bn]='\0';
+				strcpy(code[i],buf);
+			}
+			break;
+		case 1:
+			tree[i].w=2;
+			if (-1!=tree[i].rchild)
+			{
+				buf[bn++]='0';
+				i=tree[i].rchild;
+			}
+			break;
+		case 2:
+			tree[i].w=3;
+			bn--;
+			i=tree[i].parent;
+			break;
+		}
+	}
+	free(buf);
+}
+
+void fillNextReal(char *s,int n,int nextReal[]){
+	int i=0;
+	int j=-1;
+	nextReal[i]=j;
+
+	while (i<n-1)
+	{
+		if (-1==j || s[j] == s[i])
+		{
+			i++;
+			j++;
+			if (s[i]!=s[j])
+			{
+				nextReal[i]=j;
+			}else{
+				nextReal[i]=nextReal[j];		
+			}
+		}else{
+			j=nextReal[j];
+		}
+	}
+}
+
+int indexOfStr(char *s,char *cmp,int nextval[]){
+	int i=0;
+	int j=0;
+
+	while ('\0'!=s[i] && '\0'!=cmp[j])
+	{
+		if (-1==j || cmp[j]==s[i])
+		{
+			i++;
+			j++;
+		}else{
+			j=nextval[j];
+		}
+	}
+	if ('\0'!=cmp[j])
+	{
+		return -1;
+	}
+	return i-j;
+}
 //
 //int main(){
-//	int data[11];
-//	int equal[][2]={{1,2},{1,3},{4,5},{5,6},{7,8},{8,9}};
-//	partation_equal(data,11,equal,6);
-//	showSet(data,11);
+////	int data[11];
+////	int equal[][2]={{1,2},{1,3},{4,5},{5,6},{7,8},{8,9}};
+////	partation_equal(data,11,equal,6);
+//// 	showSet(data,11);
+//
+////	int w[]={1,2,3,4,5,6};
+////	char *code[6];
+////	hfmCode(w,6,code);
+////	for (int i=0;i<6;i++)
+////	{
+////		printf("%s\n",code[i]);
+//// 	}
+//	char *s="aaaab";
+//	int next[5];
+//	fillNextReal(s,5,next);
+//	for (int i=0;i<5;i++)
+//	{
+//		printf("%d ",next[i]);
+//	}
+//	printf("\n");
+//
+//	printf("%d\n",indexOfStr("aabaaaab",s,next));
 //	return 0;
 //}
